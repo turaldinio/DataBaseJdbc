4dedf927faf68d129ee73013a9dadc377c59bb5c
Tural Guluev <tural-98@bk.ru>
2022-12-20 14:26:29 +0300

    Initial commit

---
+++ src/main/java/com/example/sqljdbc_spring/SqlJdbcSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.sqljdbc_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlJdbcSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SqlJdbcSpringApplication.class, args);
+    }
+}
+
---
+++ src/test/java/com/example/sqljdbc_spring/SqlJdbcSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqljdbc_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlJdbcSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
