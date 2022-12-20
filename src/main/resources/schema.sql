63fba9aaea3ead958ecc30509956a868f715897b
Tural Guluev <tural-98@bk.ru>
2022-12-20 16:56:37 +0300

    added scheme.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,6 +7,11 @@ create table customers
     phone_number long
 
 );
+insert into customers (name, surname, age, phone_number)
+VALUES ('Alexey', 'Gavrilov', 22, 75836273948),
+       ('Dmitry', 'Lolipov', 43, 26173647564),
+       ('Pavlov', 'Sergey', 24, 2637283948);
+
 create table orders
 (
     id           int auto_increment primary key,
@@ -15,4 +20,7 @@ create table orders
     product_name char(30) not null,
     amount       int      not null,
     foreign key (customer_id) references customers (id)
-)
+);
+insert into orders (customer_id, product_name, amount)
+VALUES (1, 'MacBook air 13', 87400),
+       (2, 'Iphone 13 Pro Max', 95000);
