616cc55e3bfd9e177942867b1da6ecb47a4f0691
Tural Guluev <tural-98@bk.ru>
2022-12-20 16:10:30 +0300

    code clean up

--- src/main/java/com/example/sqljdbc_spring/DataBase.java
+++ src/main/java/com/example/sqljdbc_spring/DataBase.java
@@ -9,7 +9,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
