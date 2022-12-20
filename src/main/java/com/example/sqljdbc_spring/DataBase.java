d125eab60e7590ea56028c6ec04b7325f2a0cb95
Tural Guluev <tural-98@bk.ru>
2022-12-20 14:39:22 +0300

    added read method in DataBase class

--- src/main/java/com/example/sqljdbc_spring/DataBase.java
+++ src/main/java/com/example/sqljdbc_spring/DataBase.java
@@ -1,8 +1,22 @@
 package com.example.sqljdbc_spring;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Repository;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 @Repository
 public class DataBase {
-    
+    private static String read(String scriptFileName) {
+        try (InputStream is = new ClassPathResource(scriptFileName).getInputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is))) {
+            return bufferedReader.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
