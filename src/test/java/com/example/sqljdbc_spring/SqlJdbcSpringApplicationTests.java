4d88dd1de373164f13890f09a4833ea1abab855b
Tural Guluev <tural-98@bk.ru>
2022-12-20 16:18:13 +0300

    update tests

--- src/test/java/com/example/sqljdbc_spring/SqlJdbcSpringApplicationTests.java
+++ src/test/java/com/example/sqljdbc_spring/SqlJdbcSpringApplicationTests.java
@@ -4,16 +4,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SqlJdbcSpringApplicationTests {
+    @LocalServerPort
+    int serverPort;
+
     @Autowired
     private DataBaseService dataBaseService;
 
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
     @Test
     void contextLoads() {
-        Assertions.assertEquals(dataBaseService.getProductName("Alexey"),"MacBook air 13");
+        var result = testRestTemplate.getForEntity(String.format("http://localhost:%d/products/fetch-product?name=alexey", serverPort), String.class);
+
+        Assertions.assertEquals(result.getBody(), "MacBook air 13");
     }
 
 }
