547f37cc7849d4ff11b65345e969c1759c0a12da
Tural Guluev <tural-98@bk.ru>
2022-12-22 18:27:15 +0300

    the application is work

--- src/main/resources/db/changelog/migrations/002-insert_in_customers.sql
+++ src/main/resources/db/changelog/migrations/002-insert_in_customers.sql
@@ -1,4 +1,4 @@
-insert into customers (name, surname, age, phone_number)
+insert into netology.customers (name, surname, age, phone_number)
 VALUES ('Alexey', 'Gavrilov', 22, 75836273948),
        ('Dmitry', 'Lolipov', 43, 26173647564),
        ('Pavlov', 'Sergey', 24, 2637283948);
