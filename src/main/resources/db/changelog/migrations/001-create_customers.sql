740aa9be933f146df570e780f99b62ea07ef2d46
Tural Guluev <tural-98@bk.ru>
2022-12-22 17:23:02 +0300

    added all necessary files

---
+++ src/main/resources/db/changelog/migrations/001-create_customers.sql
@@ -0,0 +1,9 @@
+create table customers
+(
+    id           int auto_increment primary key,
+    name         char(20) not null,
+    surname      char(20) not null,
+    age          int unsigned,
+    phone_number long
+
+);
---
+++ src/main/resources/db/changelog/migrations/004-insert_in_orders.sql
@@ -0,0 +1,3 @@
+insert into orders (customer_id, product_name, amount)
+VALUES (1, 'MacBook air 13', 87400),
+       (2, 'Iphone 13 Pro Max', 95000);
